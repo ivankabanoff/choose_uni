fb3b34f25d4e9e1c52775fce2260919f783daeeb
Ivan Kabanov <kabanov_oi197@mail.ru>
2024-04-26 19:47:19 +0300

    structure changed

--- data/programs.py
+++ data/programs.py
@@ -1,17 +1,20 @@
 import datetime
 import sqlalchemy
+from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class Programs(SqlAlchemyBase):
     __tablename__ = 'programs'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String)
-    university = sqlalchemy.Column(sqlalchemy.String)
+    university_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'), index=True)
     faculty = sqlalchemy.Column(sqlalchemy.String)
     ege = sqlalchemy.Column(sqlalchemy.String)
     price = sqlalchemy.Column(sqlalchemy.Integer)
-    type = sqlalchemy.Column(sqlalchemy.String, index=True)
-    about = sqlalchemy.Column(sqlalchemy.String)
+    region = sqlalchemy.Column(sqlalchemy.String, index=True)
+    city = sqlalchemy.Column(sqlalchemy.String, index=True)
+    link = sqlalchemy.Column(sqlalchemy.String)
+    admin = orm.relationship('User')
--- main.py
+++ main.py
@@ -1,15 +1,87 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 from data import db_session
+from data.users import User
+from data.programs import Programs
+from flask_login import LoginManager, login_user, login_required, logout_user
+from forms.loginform import LoginForm
+from forms.user import RegisterForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'artur_dmitrievich_is_the_best'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Скоро здесь будет сайт для выбора вуза"
+    db_sess = db_session.create_session()
+    programs = db_sess.query(Programs)
+    return render_template('index.html', programs=programs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+def main():
+    db_session.global_init("db/programs.db")
+    app.run(port=8080, host='127.0.0.1')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 if __name__ == '__main__':
-    # db_session.global_init("db/uni_db.db")
-    app.run(port=8080, host='127.0.0.1')
+    main()
